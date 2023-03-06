121b114913edcff97b21034d290f6970a3e60cd3
Andrea-Agosta <andrea.agosta85@gmail.com>
2023-03-06 17:10:47 +0100

    create a docker container with postgress, init db, and create a dbSeeder to fill the db

--- app.ts
+++ app.ts
@@ -3,6 +3,11 @@ import express, { Application, Request, Response, NextFunction } from 'express';
 
 const app: Application = express();
 
+// app.use('/api/user', user);
+// app.use('/api/auth', auth);
+// app.use('/api/product', product);
+// app.use('/api/store', passport.authenticate('jwt', { session: false }), store);
+
 app.get("/", (_req: Request, res: Response) => {
   res.json({ "message": "Ok" })
 });
---
+++ containerConfig/initSql.sql
@@ -0,0 +1,37 @@
+DROP TABLE IF EXISTS public.flights;
+
+CREATE TABLE public.route(
+  route_id VARCHAR(50) PRIMARY KEY,
+  departure_destination VARCHAR(50) NOT NULL,
+  arrival_destination VARCHAR(50) NOT NULL
+);
+
+ALTER TABLE public.route OWNER to postgres;
+
+CREATE TABLE public.itineraries(
+  flight_id VARCHAR(50) NOT NULL,
+  -- departure_at VARCHAR(50) NOT NULL,
+  -- arrival_at VARCHAR(50) NOT NULL,
+  departure_at TIMESTAMPTZ NOT NULL,
+  arrival_at TIMESTAMPTZ NOT NULL,
+  available_seats INT NOT NULL,
+  itineraries_id VARCHAR(50),
+  CONSTRAINT fk_route
+    FOREIGN KEY(itineraries_id) 
+	    REFERENCES route(route_id),
+  CONSTRAINT unique_flight_id UNIQUE (flight_id)
+);
+
+ALTER TABLE public.itineraries OWNER to postgres;
+
+CREATE TABLE public.price(
+  currency VARCHAR(3) NOT NULL,
+  adult NUMERIC(6,2) NOT NULL,
+  child NUMERIC(6,2) NOT NULL,
+  price_id VARCHAR(50),
+  CONSTRAINT fk_itineraries
+    FOREIGN KEY(price_id) 
+	    REFERENCES itineraries(flight_id)
+);
+
+ALTER TABLE public.price OWNER to postgres;
